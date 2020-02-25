8c862f3478c044be42781604a99f4d5bbe8f733e
seedy1 <jobe.seedy@gmail.com>
2020-02-25 02:44:06 +0000

    renamed files

--- text_generation_with_spacy.py
+++ text_generation_with_spacy.py
@@ -16,11 +16,11 @@ from keras.preprocessing.sequence import pad_sequences
 
 from pickle import dump, load
 
-
 #
-# from random import randint
+from random import randint, seed
 # from bs4 import BeautifulSoup
-# from keras.models import load_model
+from keras.models import load_model
+
 
 # load data
 def read_file(file_name):
@@ -149,26 +149,75 @@ seq_length = X.shape[1]
 # vocab_size=128 + 1
 # ------------#
 
-def init_model(vocabulary_size, sequeunce_length):
-    model = Sequential()
-    model.add(Embedding(vocabulary_size, 25, input_length=sequeunce_length))
-    model.add(LSTM(150, return_sequences=True))
-    model.add(LSTM(150))
-    model.add(Dense(150, activation='relu'))
-    model.add(Dense(vocabulary_size, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    model.summary()
-    return model
-
+# def init_model(vocabulary_size, sequeunce_length):
+#     model = Sequential()
+#     model.add(Embedding(vocabulary_size, 25, input_length=sequeunce_length))
+#     model.add(LSTM(150, return_sequences=True))
+#     model.add(LSTM(150))
+#     model.add(Dense(150, activation='relu'))
+#     model.add(Dense(vocabulary_size, activation='softmax'))
+#     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#
+#     model.summary()
+#     return model
+#
 
 # SET UP AND TRAINING
-model = init_model(vocabulary_size + 1, seq_length)
+# model = init_model(vocabulary_size + 1, seq_length)
 #
 # model.fit(X, y, batch_size=128, epochs=1, verbose=1)
 
-#save model after running
+# save model after running
 # model.save('myModel.h5')
 # dump(tokenizer, open('myModel', 'wb'))
 
 print('THAT\'S ALL FLOCKS')
+print('----------------------')
+print('')
+
+
+# model.load_weights('MyModel.h5')
+
+
+def generate_text(modelm, tokenizerm, seq_lenm, seed_textm, num_gen_wordsm):
+    output_text = []
+    input_text = seed_text
+
+    for i in range(num_gen_wordsm):
+        encoded_text = tokenizerm.texts_to_sequences([input_text])[0]
+        pad_encoded = pad_sequences([encoded_text], maxlen=seq_lenm, truncating='pre')
+
+        pred_word_ind = model.predict_classes(pad_encoded, verbose=0)[0]
+        pred_word = tokenizerm.index_word[pred_word_ind]
+
+        input_text += ' ' + pred_word
+        output_text.append(pred_word)
+
+        return ' '.join(output_text)
+
+
+# print()
+seed(101)
+random_pick = randint(0, len(text_seq))
+random_seed_text = text_seq[random_pick]
+seed_text = ' '.join(random_seed_text)
+# print(seed_text)
+print('-------------------')
+# print(generate_text(model, tokenizer, seq_length, seed_text=seed_text, num_gen_words=10))
+
+# generate_text(model, tokenizer, seq_length, seed_text=seed_text, num_gen_words=50)
+
+# full_text = read_file('moby_dick_four_chapters.txt.txt')
+
+# txtx = "moby_dick_four_chapters.txt"
+# full_text = open(txtx, 'r', encoding='utf-8').read()
+#
+# for i, word in enumerate(full_text.split()):
+#     if word == 'inkling':
+#         print(' '.join(full_text.split()[i - 20:i + 20]))
+#         print('\n')
+
+model = load_model('epochBIG.h5')
+tokenizer = load(open('epochBIG', 'rb'))
+
+print( generate_text(model, tokenizer, seq_length, seed_text, num_gen_wordsm=20) )
